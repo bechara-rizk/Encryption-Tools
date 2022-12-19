f79d5b6c0c381e1a7491a93702a343b424023d08
Bechara Rizk <becharaerizk@yahoo.com>
2022-12-19 16:39:09 +0200

    implemented exponentiation and extended euclid

---
+++ tools/exponentiation.py
@@ -0,0 +1,21 @@
+
+
+def exponentiation(base, exponent, mod):
+    exponent=bin(exponent)[2:]
+    result=1
+    for i in exponent:
+        result=(result*result)%mod
+        if i=='1':
+            result=(result*base)%mod
+    return result
+
+
+if __name__=='__main__':
+    print(exponentiation(983,19271893,19019))
+    print(exponentiation(984,19271893,19019))
+    print(exponentiation(985,19271893,19019))
+    print(exponentiation(986,19271893,19019))
+    print(exponentiation(987,19271893,19019))
+    print(exponentiation(988,19271893,19019))
+    print(exponentiation(989,19271893,19019))
+    print(exponentiation(990,19271893,19019))
---
+++ tools/extendedEuclid.py
@@ -0,0 +1,24 @@
+
+
+def extendedEuclid(inverse, mod):
+    A=(1,0,mod)
+    B=(0,1,inverse)
+    while True:
+        if B[2]==0:
+            return 'no inverse'
+        if B[2]==1:
+            return B[1]
+        Q=A[2]//B[2]
+        C=((A[0]-Q*B[0])%mod,(A[1]-Q*B[1])%mod,(A[2]-Q*B[2])%mod)
+        A=B
+        B=C
+
+if __name__=='__main__':
+    print(extendedEuclid(983,19019))
+    print(extendedEuclid(984,19019))
+    print(extendedEuclid(985,19019))
+    print(extendedEuclid(986,19019))
+    print(extendedEuclid(987,19019))
+    print(extendedEuclid(988,19019))
+    print(extendedEuclid(989,19019))
+    print(extendedEuclid(990,19019))
